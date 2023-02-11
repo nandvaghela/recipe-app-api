2e206cb8356036cd41b4e93f6d6a0269de4b4c1d
nandvaghela <45141940+nandvaghela@users.noreply.github.com>
2023-02-11 15:14:52 -0500

    Added custom user model with tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,37 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,BaseUserManager,PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user"""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return new user"""
+        if not email:
+           raise ValueError('User mush have an email address.')
+        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self,email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 # Create your models here.
+class User(AbstractBaseUser,PermissionsMixin):
+    """User in our system."""
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
